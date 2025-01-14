9fa04cd65278afafbe35fc44fad4f5dcb6a08707
Ishaan Verma <ishaan56verma@gmail.com>
2025-01-15 00:16:24 +0530

    HttpRequest refactor. thank you deepseek.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use::std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream};
 use std::io::{BufRead, BufReader, Write};
 use std::fs;
 use std::thread;
@@ -8,8 +8,6 @@ use std::time::Duration;
 
 pub mod threadpool;
 use threadpool::ThreadPool;
-
-
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum HttpError {
@@ -30,48 +28,57 @@ impl HttpError {
 
 #[derive(PartialEq, Eq, Hash, Debug, Clone)]
 enum HttpMethod {
-  GET(String),
+  GET,
+  POST,
+  PUT,
+  DELETE,
 }
 
-impl HttpMethod {
-  fn from_header(req: &Vec<String>) -> Option<HttpMethod> {
-    if req.len() == 0 { return None; }
+#[derive(Debug, Clone)]
+struct HttpRequest {
+  method: HttpMethod,
+  uri: String,
+}
+
+impl HttpRequest {
+  fn from_header(req: &Vec<String>) -> Option<HttpRequest> {
+    if req.len() == 0 {
+      return None;
+    }
     let mut words = req[0].split_whitespace();
     let m = words.next().unwrap();
     let r = words.next().unwrap().to_string();
     match m {
-      "GET" => Some(HttpMethod::GET(r)),
+      "GET" => Some(HttpRequest {
+        method: HttpMethod::GET,
+        uri: r,
+      }),
+      "POST" => Some(HttpRequest {
+        method: HttpMethod::POST,
+        uri: r,
+      }),
+      "PUT" => Some(HttpRequest {
+        method: HttpMethod::PUT,
+        uri: r,
+      }),
+      "DELETE" => Some(HttpRequest {
+        method: HttpMethod::DELETE,
+        uri: r,
+      }),
       _ => None,
     }
   }
 }
 
 type HttpResponse = Result<String, HttpError>;
-type LogEntry = (Option<HttpMethod>, HttpResponse);
-
-struct HttpLogger {
-  log: Vec<LogEntry>,
-}
-
-impl HttpLogger {
-  fn new() -> Self { HttpLogger { log: Vec::new() } }
-
-  fn push(&mut self, method: &Option<HttpMethod>, resp: &HttpResponse) {
-    let entry = (method.clone(), resp.clone());
-    println!("{:?}", entry);
-    self.log.push(entry);
-  }
-}
-
 type RouteFn = threadpool::JobFn<TcpStream>;
 type Job = (TcpStream, RouteFn);
 
 struct HttpServer {
   addr: String,
-  routes: HashMap<HttpMethod, RouteFn>,
+  routes: HashMap<(HttpMethod, String), RouteFn>,
   error_handlers: HashMap<HttpError, RouteFn>,
   threadpool: ThreadPool<TcpStream>,
-  logger: HttpLogger,
 }
 
 impl HttpServer {
@@ -82,25 +89,27 @@ impl HttpServer {
       HttpError::NotFound,
       Arc::new(|mut stream| {
         stream.write_all("404".as_bytes()).unwrap();
-      }));
-    
+      }),
+    );
+
     error_handlers.insert(
       HttpError::BadRequest,
       Arc::new(|mut stream| {
         stream.write_all("400".as_bytes()).unwrap();
-      }));
-    
+      }),
+    );
+
     error_handlers.insert(
       HttpError::InternalServerError,
       Arc::new(|mut stream| {
         stream.write_all("500".as_bytes()).unwrap();
-      }));
-    
+      }),
+    );
+
     HttpServer {
       addr: addr.to_string(),
       routes: HashMap::new(),
       threadpool: ThreadPool::<TcpStream>::new(5),
-      logger: HttpLogger::new(),
       error_handlers,
     }
   }
@@ -113,8 +122,9 @@ impl HttpServer {
     Err(err)
   }
 
-  fn route(&mut self, stream: TcpStream, method: HttpMethod) -> HttpResponse {
-    match self.routes.get(&method) {
+  fn route(&mut self, stream: TcpStream, req: HttpRequest) -> HttpResponse {
+    let key = (req.method.clone(), req.uri.clone());
+    match self.routes.get(&key) {
       Some(f) => {
         self.threadpool.execute(Arc::clone(f), stream);
         Ok("NotImplement: thread not joining".to_string())
@@ -131,13 +141,17 @@ impl HttpServer {
       .take_while(|line| !line.is_empty())
       .collect::<Vec<_>>();
 
-    let (method, resp): (Option<HttpMethod>, HttpResponse) =
-      match HttpMethod::from_header(&req) {
-        Some(m) => (Some(m.clone()), self.route(stream, m.clone())),
-        None => (None, self.handle_error(stream, HttpError::BadRequest)),
-      };
-
-    self.logger.push(&method, &resp);
+    match HttpRequest::from_header(&req) {
+      Some(r) => {
+        println!("Request: {:?}", r);
+        let resp = self.route(stream, r.clone());
+        println!("Response: {:?}", resp);
+      },
+      None => {
+        println!("Bad Request");
+        let _ = self.handle_error(stream, HttpError::BadRequest);
+      },
+    }
   }
 
   fn serve(&mut self) {
@@ -158,16 +172,16 @@ impl HttpServer {
     format!("HTTP/1.1 {status}\r\nContent-length: {len}\r\n\r\n{content}")
   }
 
-  fn add_route(&mut self, m: HttpMethod, f: fn(HttpMethod) -> HttpResponse) {
+  fn add_route(&mut self, m: HttpMethod, uri: String, f: fn(HttpRequest) -> HttpResponse) {
     self.routes.insert(
-      m.clone(),
+      (m.clone(), uri.clone()),
       Arc::new(move |mut stream: TcpStream| {
-        stream.write_all(Self::postprocess_response(f(m.clone())).as_bytes()).unwrap();
-      })
+        let req = HttpRequest { method: m.clone(), uri: uri.clone() };
+        stream.write_all(Self::postprocess_response(f(req)).as_bytes()).unwrap();
+      }),
     );
   }
 }
-
 
 fn ok_html(filename: &str) -> HttpResponse {
   match fs::read_to_string(filename) {
@@ -180,14 +194,10 @@ fn main() {
   use HttpMethod::*;
   let mut app = HttpServer::new("127.0.0.1:8080");
 
-
-  app.add_route(GET("/".to_string()),
-                |_| ok_html("hello.html"));
-  
-  app.add_route(GET("/sleep".to_string()),
-                |_| {
-                  thread::sleep(Duration::from_secs(5));
-                  ok_html("hello.html")});
+  app.add_route(GET, "/".to_string(), |_| ok_html("hello.html"));
+  app.add_route(GET, "/sleep".to_string(), |_| {
+    thread::sleep(Duration::from_secs(5));
+    ok_html("hello.html")
+  });
   app.serve();
 }
-
